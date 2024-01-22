d71afcc984b0bb2d3f04a133dd8d0e85cf360f0d
root <andrewomosh44@gmail.com>
2024-01-22 21:28:53 +0300

    fix pip-setuptools

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/code-x/Desktop/Dev-Jan.24/tbc_api/venv"
+set -gx VIRTUAL_ENV "/home/code-x/Desktop/Dev-Jan/tbc_api/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
